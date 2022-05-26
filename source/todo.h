576a5b0fdd991dd3e3f66c443e4f689cccaad012
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-26 20:18:57 +0430

    initilize todo header file

---
+++ source/todo.h
@@ -0,0 +1,14 @@
+#ifndef TODO
+#define TODO
+
+static int posInList = 0;
+static int listCount = 0;
+
+struct ToDo
+{
+    std::string description;
+    std::string currentDate;
+    int priority;
+};
+
+#endif // TODO
