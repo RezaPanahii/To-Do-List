d5f5842725b7af87894597ac17883847228c506b
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-27 02:53:17 +0430

    modify menu for delete function

--- source/App.cpp
+++ source/App.cpp
@@ -5,6 +5,7 @@
 
 bool Add(const std::string& desc, const int& priority);
 void List();
+bool Delete(const std::string& desc);
 
 int main()
 {
@@ -27,7 +28,7 @@ int main()
         std::cout << "\n";
         std::cout << "\t\t\tEnter your choice : ";
         std::cin >> choice;
-
+        
         if (choice == 1) {
             std::string description;
             int priority;
@@ -37,11 +38,25 @@ int main()
             std::cout << "Enter task priority : (1 for lowest priority and 5 for highest priority) \n";
             std::cin >> priority;
 
-            Add(description, priority);
-
+            if (Add(description, priority)) {
+                std::cout << "Added successfully!" << std::endl;
+            }
         } else if (choice == 2) {
             List();
-        } else {
+        }
+        else if (choice == 3) {
+            std::string description;
+            
+            std::cout << "Enter the task you want to delete : \n";
+            std::getline(std::cin, description);
+            if (Delete(description)) {
+                std::cout << "Deleted successfully!" << std::endl;
+            }
+            else {
+                std::cout << "Description not found!" << std::endl;
+            }
+        }
+        else {
             std::cout << "Wrong input!" << std::endl;
         }
     } while (choice != 0);
