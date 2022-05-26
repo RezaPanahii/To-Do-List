3704f366f83cbc7683a765ecc6e386a47c0ea885
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-26 20:35:16 +0430

    create list function

--- source/todo.cpp
+++ source/todo.cpp
@@ -6,4 +6,11 @@ struct ToDo ToDoList[100];
 void Add();
 void Delete();
 void Modify();
-void List();
+void List() {
+    for (int i = 0; i < listCount; i++) {
+        std::cout << "description : " << ToDoList[i].description << std::endl;
+        std::cout << "date : " << ToDoList[i].current_date << std::endl;
+        std::cout << "priority : " << ToDoList[i].priority << std::endl;
+        std::cout << "" << std::endl;
+    }
+}
